9f3188c72b752d22660c4632057b95f2b9803e00
ziya.mammadov.r@gmail.com <ziya584@bitbucket.org>
2019-04-18 11:28:37 +0300

    Добавил файл конфигурации и объявил константы

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$localhost = 'localhost';
+$user = 'root';
+$password = '';
+$dbname = 'geekbrains';
+
+	define('HOST', "localhost");
+	define('USER', "root");
+	define('PASSWORD', "");
+	define('DB', "geekbrains");
+	define('CHARSET', "utf8");
