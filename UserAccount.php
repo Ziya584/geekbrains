bb5769cec7f06df5df540b9195cbdc57f4a33a7f
ziya.mammadov.r@gmail.com <ziya584@bitbucket.org>
2019-04-19 15:47:49 +0300

    Добавил страницу просмотра отдельного товара

--- UserAccount.php
+++ UserAccount.php
@@ -11,7 +11,7 @@
 		<ul class="list-unstyled">
 			<li class="media">
 				<img src="<?= $offer['offer_img_small'] ?>" class="mr-3" alt="...">
-				<a href="EditOffer.php?id=<?= $offer['id'] ?>">
+				<a href="kartochka.php?id=<?= $offer['id'] ?>">
 					<div class=" media-body">
 						<h5 class="mt-0 mb-1"><?= $offer['title']; ?></h5>
 						<p><?= $offer['short_description']; ?></p>
@@ -19,6 +19,12 @@
 						<form action="korzinaAdd.php" method="POST">
 							<input type="hidden" name="offer_id" value="<?=$offer['id']?>">
 							<input type="submit" name="korzina" value="В корзину">
+						</form>
+						<form action="kartochka.php" method="POST">
+							<input type="hidden" name="offer_title" value="<?=$offer['title']?>">
+							<input type="hidden" name="offer_desc" value="<?=$offer['full_description']?>">
+							<input type="hidden" name="offer_img" value="<?=$offer['offer_img_small']?>">
+							<input type="submit" name="kartochka" value="Подробнее">
 						</form>
 					</div>
 				</a>
